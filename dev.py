6f2b34c99dfa6239e92011a11260164981938f68
Juan Imbett <jfimbett@gmail.com>
2024-10-21 15:24:45 +0200

    feat: basic blockchain implementation

--- dev.py
+++ dev.py
@@ -1,16 +1,24 @@
 #%%
-from pybacktestchain.data_module import  FirstTwoMoments
+from pybacktestchain.data_module import FirstTwoMoments
 from pybacktestchain.broker import Backtest, StopLoss
+from pybacktestchain.blockchain import load_blockchain
 from datetime import datetime
 
+# Set verbosity for logging
+verbose = False  # Set to True to enable logging, or False to suppress it
+
 backtest = Backtest(
-    initial_date = datetime(2010, 1, 1),
-    final_date = datetime(2020, 1, 1),
-    information_class = FirstTwoMoments,
-    risk_model=StopLoss
-    )
+    initial_date=datetime(2019, 1, 1),
+    final_date=datetime(2020, 1, 1),
+    information_class=FirstTwoMoments,
+    risk_model=StopLoss,
+    name_blockchain='backtest',
+    verbose=verbose
+)
 
 backtest.run_backtest()
 
-
-# %%
+block_chain = load_blockchain('backtest')
+print(str(block_chain))
+# check if the blockchain is valid
+print(block_chain.is_valid())
--- src/pybacktestchain/broker.py
+++ src/pybacktestchain/broker.py
@@ -2,9 +2,11 @@ import pandas as pd
 import logging
 from dataclasses import dataclass
 from datetime import datetime
+import os 
+import pickle
 from pybacktestchain.data_module import UNIVERSE_SEC, FirstTwoMoments, get_stocks_data, DataModule, Information
 from pybacktestchain.utils import generate_random_name
-
+from pybacktestchain.blockchain import Block, Blockchain
 
 # Setup logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -25,6 +27,25 @@ class Broker:
     positions: dict = None
     transaction_log: pd.DataFrame = None
     entry_prices: dict = None
+    verbose: bool = True
+
+    def initialize_blockchain(self, name: str):
+        # Check if the blockchain is already initialized and stored in the blockchain folder
+        chains = os.listdir('blockchain')
+        ending = f'{name}.pkl'
+        if ending in chains:
+            if self.verbose:
+                logging.warning(f"Blockchain with name {name} already exists. Please use a different name.")
+            with open(f'blockchain/{name}.pkl', 'rb') as f:
+                self.blockchain = pickle.load(f)
+            return
+
+        self.blockchain = Blockchain(name)
+        # Store the blockchain
+        self.blockchain.store()
+
+        if self.verbose:
+            logging.info(f"Blockchain with name {name} initialized and stored in the blockchain folder.")
 
     def __post_init__(self):
         # Initialize positions as a dictionary of Position objects
@@ -38,32 +59,24 @@ class Broker:
         if self.entry_prices is None:
             self.entry_prices = {}
 
-    def get_cash_balance(self):
-        """Returns the current cash balance."""
-        return self.cash
-
     def buy(self, ticker: str, quantity: int, price: float, date: datetime):
         """Executes a buy order for the specified ticker."""
         total_cost = price * quantity
         if self.cash >= total_cost:
             self.cash -= total_cost
             if ticker in self.positions:
-                # Update existing position
                 position = self.positions[ticker]
                 new_quantity = position.quantity + quantity
                 new_entry_price = ((position.entry_price * position.quantity) + (price * quantity)) / new_quantity
                 position.quantity = new_quantity
                 position.entry_price = new_entry_price
             else:
-                # Create new position
                 self.positions[ticker] = Position(ticker, quantity, price)
-            # Log the transaction
             self.log_transaction(date, 'BUY', ticker, quantity, price)
-
-            # store the entry prices 
             self.entry_prices[ticker] = price
         else:
-            logging.warning(f"Not enough cash to buy {quantity} shares of {ticker} at {price}. Available cash: {self.cash}")
+            if self.verbose:
+                logging.warning(f"Not enough cash to buy {quantity} shares of {ticker} at {price}. Available cash: {self.cash}")
     
     def sell(self, ticker: str, quantity: int, price: float, date: datetime):
         """Executes a sell order for the specified ticker."""
@@ -71,14 +84,13 @@ class Broker:
             position = self.positions[ticker]
             position.quantity -= quantity
             self.cash += price * quantity
-            # If position size becomes zero, remove it
             if position.quantity == 0:
                 del self.positions[ticker]
                 del self.entry_prices[ticker]
-            # Log the transaction
             self.log_transaction(date, 'SELL', ticker, quantity, price)
         else:
-            logging.warning(f"Not enough shares to sell {quantity} shares of {ticker}. Position size: {self.positions.get(ticker, 0)}")
+            if self.verbose:
+                logging.warning(f"Not enough shares to sell {quantity} shares of {ticker}. Position size: {self.positions.get(ticker, 0)}")
     
     def log_transaction(self, date, action, ticker, quantity, price):
         """Logs the transaction."""
@@ -90,8 +102,13 @@ class Broker:
             'Price': price,
             'Cash': self.cash
         }])
-        # Concatenate the new transaction to the existing log
         self.transaction_log = pd.concat([self.transaction_log, transaction], ignore_index=True)
+
+    def get_cash_balance(self):
+        return self.cash
+
+    def get_transaction_log(self):
+        return self.transaction_log
 
     def get_portfolio_value(self, market_prices: dict):
         """Calculates the total portfolio value based on the current market prices."""
@@ -107,17 +124,16 @@ class Broker:
         for ticker, weight in portfolio.items():
             price = prices.get(ticker)
             if price is None:
-                logging.warning(f"Price for {ticker} not available on {date}")
+                if self.verbose:
+                    logging.warning(f"Price for {ticker} not available on {date}")
                 continue
             
-            # Calculate the desired quantity based on portfolio weight
             total_value = self.get_portfolio_value(prices)
             target_value = total_value * weight
             current_value = self.positions.get(ticker, Position(ticker, 0, 0)).quantity * price
             diff_value = target_value - current_value
             quantity_to_trade = int(diff_value / price)
             
-            # First, execute the sell trades (if quantity_to_trade is negative)
             if quantity_to_trade < 0:
                 self.sell(ticker, abs(quantity_to_trade), price, date)
         
@@ -125,17 +141,16 @@ class Broker:
         for ticker, weight in portfolio.items():
             price = prices.get(ticker)
             if price is None:
-                logging.warning(f"Price for {ticker} not available on {date}")
+                if self.verbose:
+                    logging.warning(f"Price for {ticker} not available on {date}")
                 continue
             
-            # Calculate the desired quantity based on portfolio weight
             total_value = self.get_portfolio_value(prices)
             target_value = total_value * weight
             current_value = self.positions.get(ticker, Position(ticker, 0, 0)).quantity * price
             diff_value = target_value - current_value
             quantity_to_trade = int(diff_value / price)
             
-            # Now, execute the buy trades (if quantity_to_trade is positive) and check if there's enough cash
             if quantity_to_trade > 0:
                 available_cash = self.get_cash_balance()
                 cost = quantity_to_trade * price
@@ -143,9 +158,9 @@ class Broker:
                 if cost <= available_cash:
                     self.buy(ticker, quantity_to_trade, price, date)
                 else:
-                    logging.warning(f"Not enough cash to buy {quantity_to_trade} of {ticker} on {date}. Needed: {cost}, Available: {available_cash}")
-                    # then buy as many shares as possible with the available cash
-                    logging.info(f"Buying as many shares of {ticker} as possible with available cash.")
+                    if self.verbose:
+                        logging.warning(f"Not enough cash to buy {quantity_to_trade} of {ticker} on {date}. Needed: {cost}, Available: {available_cash}")
+                        logging.info(f"Buying as many shares of {ticker} as possible with available cash.")
                     quantity_to_trade = int(available_cash / price)
                     self.buy(ticker, quantity_to_trade, price, date)
 
@@ -203,11 +218,15 @@ class Backtest:
     rebalance_flag : type = EndOfMonth
     risk_model : type = StopLoss
     initial_cash: int = 1000000  # Initial cash in the portfolio
+    name_blockchain: str = 'backtest'
+    verbose: bool = True
+    broker = Broker(cash=initial_cash, verbose=verbose)
 
-    broker = Broker(cash=initial_cash)
+
 
     def __post_init__(self):
         self.backtest_name = generate_random_name()
+        self.broker.initialize_blockchain(self.name_blockchain)
 
     def run_backtest(self):
         logging.info(f"Running backtest from {self.initial_date} to {self.final_date}.")
@@ -249,4 +268,7 @@ class Backtest:
         df = self.broker.get_transaction_log()
         # save to csv, use the backtest name 
         df.to_csv(f"backtests/{self.backtest_name}.csv")
+
+        # store the backtest in the blockchain
+        self.broker.blockchain.add_block(self.backtest_name, df.to_string())
     
